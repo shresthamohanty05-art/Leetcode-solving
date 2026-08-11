b01323b5613c986cdf2beeb6d5efeb66e8f0f160
apnacollege-demo <shresthamohanty05@gmail.com>
2026-08-11 21:42:10 +0530

    stack started

---
+++ Q739.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        int[] ans = new int[temperatures.length];
+          for(int i=0 ; i< temperatures.length; i++){
+ while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]){
+        int prev = stack.pop();
+        ans[prev] = i-prev;
+      }
+stack.push(i);
+          }
+
+        return ans ;
+        
+    }
+}
